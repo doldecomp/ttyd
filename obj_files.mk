e9a87141f1a31fe750bb6d527265e17c77062c74
ProjectRevoTPP <projectrevotpp@hotmail.com>
2021-01-21 15:33:31 -0500

    split up to evtmgr_cmd

--- obj_files.mk
+++ obj_files.mk
@@ -7,8 +7,28 @@ EXTAB_O_FILES :=                                    \
 EXTABINDEX_O_FILES :=                               \
     $(BUILD_DIR)/asm/extabindex.o
 
-TEXT_O_FILES := 						            \
-	$(BUILD_DIR)/asm/text.o
+TEXT_O_FILES :=                                     \
+    $(BUILD_DIR)/asm/main.o                         \
+    $(BUILD_DIR)/asm/system.o                       \
+    $(BUILD_DIR)/asm/seq_game.o                     \
+    $(BUILD_DIR)/asm/seq_mapchange.o                \
+    $(BUILD_DIR)/asm/seq_title.o                    \
+    $(BUILD_DIR)/asm/bgdrv.o                        \
+    $(BUILD_DIR)/asm/cam_road.o                     \
+    $(BUILD_DIR)/asm/camdrv.o                       \
+    $(BUILD_DIR)/asm/dispdrv.o                      \
+    $(BUILD_DIR)/asm/fadedrv.o                      \
+    $(BUILD_DIR)/asm/hitdrv.o                       \
+    $(BUILD_DIR)/asm/icondrv.o                      \
+    $(BUILD_DIR)/asm/lightdrv.o                     \
+    $(BUILD_DIR)/asm/mapdrv.o                       \
+    $(BUILD_DIR)/asm/shadowdrv.o                    \
+    $(BUILD_DIR)/asm/seqdrv.o                       \
+    $(BUILD_DIR)/asm/DEMOInit.o                     \
+    $(BUILD_DIR)/asm/DEMOPad.o                      \
+    $(BUILD_DIR)/asm/memory.o                       \
+    $(BUILD_DIR)/asm/evtmgr_cmd.o                   \
+    $(BUILD_DIR)/asm/text.o
 
 CTORS_O_FILES :=                                    \
     $(BUILD_DIR)/asm/ctors.o
