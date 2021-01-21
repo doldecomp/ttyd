672120186e6fd4a22c3cf8f6b1e968ed02e498b3
ProjectRevoTPP <projectrevotpp@hotmail.com>
2021-01-20 22:08:50 -0500

    disassemble ctors and dtors.

--- asm/ctors.s
+++ asm/ctors.s
@@ -4,4 +4,6 @@
 .section .ctors, "wa"  # 0x802C0EE0 - 0x802C0F00
 .global lbl_802C0EE0
 lbl_802C0EE0:
-	.incbin "baserom.dol", 0x2BDEE0, 0x20
+    .4byte NULL
+
+    .balign 0x20, 0
--- asm/dtors.s
+++ asm/dtors.s
@@ -4,4 +4,6 @@
 .section .dtors, "wa"  # 0x802C0F00 - 0x802C0F20
 .global lbl_802C0F00
 lbl_802C0F00:
-	.incbin "baserom.dol", 0x2BDF00, 0x20
+    .4byte func_8025E868
+
+    .balign 0x20, 0
