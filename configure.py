f976c36452c79539972ad0c7c79c74fa23037440
NWPlayer123 <NWPlayer123@users.noreply.github.com>
2025-01-30 01:55:12 -0700

    Back To The Lab Again
    OK

--- configure.py
+++ configure.py
@@ -144,8 +144,8 @@ if not config.non_matching:
 # Tool versions
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20240706"
-config.dtk_tag = "v1.3.0"
-config.objdiff_tag = "v2.5.0"
+config.dtk_tag = "v1.4.0"
+config.objdiff_tag = "v2.7.1"
 config.sjiswrap_tag = "v1.2.0"
 config.wibo_tag = "0.6.11"
 
@@ -681,8 +681,8 @@ config.libs = [
         "cflags": cflags_static,
         "progress_category": "game",
         "objects": [
-            Object(Matching, "manager/evtmgr_cmd.c"),
-            Object(Matching, "manager/evtmgr.c"),
+            Object(NonMatching, "manager/evtmgr_cmd.c"),
+            Object(NonMatching, "manager/evtmgr.c"),
             Object(NonMatching, "manager/fontmgr.c"),
             Object(NonMatching, "manager/cardmgr.c"),
             Object(NonMatching, "manager/dvdmgr.c"),
